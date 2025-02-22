558f6b5fc8b27a669b0157f696535a3785690998
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-22 15:50:39 +0530

    some patterns
    some patterns

---
+++ Day-1/Fourth_print.java
@@ -0,0 +1,24 @@
+public class Fourth_print {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if ((i==0||i==1||i==2)&&(j==0||j==4))
+                {
+                    System.out.print("*");
+                }else if(i==3 &&(j==1 || j==3)){
+                    System.out.print("*");
+                }else if(i==4&&j==2)
+                {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}

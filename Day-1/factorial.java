9bb7550c5d0dd0b99fe717f679fb822c8ec53f6a
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-23 17:10:23 +0530

    factorial

---
+++ Day-1/factorial.java
@@ -0,0 +1,13 @@
+public class factorial {
+
+    public static void main(String[] args) {
+        long ans = 1;
+        int factorial = 20;
+         for (int i=factorial; i>=1;i--)
+         {
+             ans = ans*i;
+         }
+
+        System.out.println("factorial: "+ans);
+    }
+}

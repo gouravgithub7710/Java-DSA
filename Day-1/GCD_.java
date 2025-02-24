247eee6caf8448196730f49e74a99c58cdb8b3bc
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-24 23:13:34 +0530

    GCD_

---
+++ Day-1/GCD_.java
@@ -0,0 +1,7 @@
+public class GCD_ {
+
+    public static void main(String args[])
+    {
+        
+    }
+}
---
+++ Day-1/Prime_number_check.java
@@ -0,0 +1,17 @@
+public class Prime_number_check {
+    public static void main(String[] args) {
+        int prime_num = 17;
+        int factorial = 0;
+
+        for (int i = 1; i <= prime_num; i++) {
+            if (prime_num % i == 0) {
+                factorial++;
+            }
+        }
+        if (factorial > 2) {
+            System.out.println(prime_num + " this is not a prime num");
+        } else {
+            System.out.println(prime_num + " this is prime num");
+        }
+    }
+}

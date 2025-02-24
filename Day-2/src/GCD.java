d3c9531ecb4543bfb210df27982949972af67fa0
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-25 02:07:29 +0530

    GCD_Done

--- Day-2/src/GCD.java
+++ Day-2/src/GCD.java
@@ -2,6 +2,18 @@ public class GCD {
 
     public static void main(String args[])
     {
+        int n1=24;
+        int n2=36;
 
+        while (n1%n2!=0)
+        {
+            int rem = n1%n2;
+
+            n1=n2;
+            n2=rem;
+        }
+
+
+        System.out.println(n2);
     }
 }

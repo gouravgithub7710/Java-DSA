15fd956de60488c9920393f7300477d8ca464068
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-25 02:20:08 +0530

    Reverse_String

---
+++ Day-2/src/Reverse_String.java
@@ -0,0 +1,12 @@
+public class Reverse_String {
+
+    public static void main(String[] args) {
+        String name = "Gourav";
+
+        StringBuilder sb = new StringBuilder(name);
+
+        name = sb.reverse().toString();
+
+        System.out.println(name);
+    }
+}

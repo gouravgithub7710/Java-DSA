d43fc28d30aa66effd4f0285250297f39409b131
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-26 23:28:13 +0530

    Fibonachi

--- Day-2/src/Fibonachi.java
+++ Day-2/src/Fibonachi.java
@@ -1,6 +1,23 @@
 public class Fibonachi {
 
+
+    public static void printfibo(int n){
+        int first=0,second=1,nthfibo=0;
+
+        if (n==1){ System.out.println(first); return;}
+         else if (n==2) {System.out.println(first+" "+second); return;}
+
+         for (int i=2;i<=n;i++)
+         {
+             nthfibo = first+second;
+
+             System.out.print(nthfibo+" ");
+             second=nthfibo;
+             first=second;
+         }
+    }
     public static void main(String[] args) {
 
+        printfibo(3);
     }
 }

7401925df4a83bc29123429292bfe32cbb850869
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-02-26 23:54:17 +0530

    update GCD and fibo

--- Day-2/src/GCD.java
+++ Day-2/src/GCD.java
@@ -5,15 +5,18 @@ public class GCD {
         int n1=24;
         int n2=36;
 
-        while (n1%n2!=0)
-        {
-            int rem = n1%n2;
+       while (n1%n2!=0)
+       {
+           int rem=n1%n2;
 
-            n1=n2;
-            n2=rem;
-        }
+           n1=n2;
 
+           n2=rem;
+
+       }
 
         System.out.println(n2);
+
+//----see copy for understanding or using this yt: https://www.youtube.com/watch?v=ZtDeDD1VYLk
     }
 }

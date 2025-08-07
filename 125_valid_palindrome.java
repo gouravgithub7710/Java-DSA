6054a4eab22dea3491d6ce1149c0f8d982b27e4f
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2025-08-07 23:57:50 +0530

    Update 125_valid_palindrome.java - LeetCode Problem 125

--- 125_valid_palindrome.java
+++ 125_valid_palindrome.java
@@ -1,19 +1,28 @@
-public boolean isPalindrome(String s) {
-    int left = 0, right = s.length() - 1;
-    
-    while (left < right) {
-        while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            // Agar left character letter ya digit nahi hai, to skip karo
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // Agar right character letter ya digit nahi hai, to skip karo
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Agar characters equal nahi hain (case-insensitive), to false return karo
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            // Move both pointers
             left++;
-        }
-        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
             right--;
         }
-        if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-            return false;
-        }
-        left++;
-        right--;
+
+        return true;
     }
-    
-    return true;
 }

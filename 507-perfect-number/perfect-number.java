3f55384ab0ada712f3f098740354af3494fdc5b7
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2025-08-15 23:26:36 +0530

    Time: 2108 ms (15.93%) | Memory: 40.7 MB (29.84%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,16 +1,33 @@
-class Solution {
-    public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        int cpy = num;
-        for(int i=1; i<num;i++)
-        {
-            if(num%i==0)
-            {
-                sum = sum+i;
-            }
-        }
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         int sum = 0;
+//         int cpy = num;
+//         for(int i=1; i<num;i++)
+//         {
+//             if(num%i==0)
+//             {
+//                 sum = sum+i;
+//             }
+//         }
 
-        if(cpy==sum){ return true;}
-        return false;
+//         if(cpy==sum){ return true;}
+//         return false;
+//     }
+// }
+
+
+public class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+
+        }
+        return sum == num;
     }
 }
